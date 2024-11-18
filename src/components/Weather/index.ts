0e3c0e5b3a63fc36c5684c8ed3081f33f8851f74
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-11-18 11:32:10 +0100

    skipped this

---
+++ src/components/Weather/index.ts
@@ -0,0 +1,3 @@
+export { default as LocalDateAndTime } from "./local-date-and-time";
+export { default as WeatherDetails } from "./weather-details";
+export { default as ForeCast } from "./forecast";
