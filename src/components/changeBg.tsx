4d454e77a0deda37b778ab5c490b8ec679dcfcfd
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-11-18 11:35:43 +0100

    new file placement

---
+++ src/components/changeBg.tsx
@@ -0,0 +1,53 @@
+interface WeatherData {
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+}
+
+const changeBg = (imagePath: string, color: string) => {
+  document.documentElement.style.setProperty("--accent", `var(--${color})`);
+  document.body.style.background = `url(${imagePath}) no-repeat center center`;
+  document.body.style.backgroundSize = "cover";
+};
+
+const changeDefaultBg = () => {
+  let defaultBg: number | string = localStorage.getItem("defaultBg") || "4";
+  if (typeof defaultBg === "string" && defaultBg === "-1") {
+    defaultBg = Math.floor(Math.random() * 6).toString();
+    localStorage.setItem("defaultBg", defaultBg);
+  }
+  const imagePath = `/WeatherWatch/img/default/img${defaultBg}.jpg`;
+  const color = `img${defaultBg}`;
+
+  changeBg(imagePath, color);
+};
+
+const changeDynamicBg = (data: WeatherData) => {
+  let imagePath, color;
+  let status = localStorage.getItem("dynamicBg") || "ON";
+  if (status === "OFF") return;
+
+  let dynamicBg = data.weather[0].main.toLowerCase();
+  let day_night = data.weather[0].icon[2];
+
+  if (
+    dynamicBg === "clear" ||
+    dynamicBg === "clouds" ||
+    dynamicBg === "rain" ||
+    dynamicBg === "snow"
+  ) {
+    imagePath = `/WeatherWatch/img/weather/${day_night}-${dynamicBg}.jpg`;
+    color = `${day_night}-${dynamicBg}`;
+  } else {
+    if (dynamicBg == "sand" || dynamicBg == "ash") dynamicBg = "dust";
+
+    imagePath = `/WeatherWatch/img/weather/${dynamicBg}.jpg`;
+    color = `${dynamicBg}`;
+  }
+  changeBg(imagePath, color);
+};
+
+export { changeDefaultBg, changeDynamicBg };
